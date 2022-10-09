e9f0278b2c22f9dcfe9dfacbb32e958b8788f531
ROMAIN <sebastien.romain@econocom.com>
2022-10-09 18:45:29 +0200

    initial commit

---
+++ src/test/java/fr/sromain/pockafka/PocKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.sromain.pockafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
